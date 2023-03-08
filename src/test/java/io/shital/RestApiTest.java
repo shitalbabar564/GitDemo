5f536f191e651bf299c7f16cf5effeb3e329a86c
shital <shitalbabar08@gmail.com>
2023-03-08 12:19:25 +0530

    This is a commit from Assian timezone guy

---
+++ src/test/java/io/shital/RestApiTest.java
@@ -0,0 +1,20 @@
+package io.shital;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	@Test
+	public void postJira() {
+		System.out.println("postJira");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+
+	}
+
+	@Test
+	public void deleteTwitter() {
+		System.out.println("delete twitter");
+
+	}
+
+}
